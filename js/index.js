0be22d9d120a12385ce05eb79a64a2ce2169c181
riddos <115896659+riddos@users.noreply.github.com>
2025-01-13 22:37:05 +0100

    Update and rename scriptss.js to index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        const menuToggle = document.querySelector(".menu-toggle");
+        const menu = document.querySelector("nav ul.menu");
+
+        menuToggle.addEventListener("click", () => {
+            menu.classList.toggle("active");
+        });
+    });
+</script>
