f0c9094abf9e61bec38d8f6edcecf4dc6d7a46dd
riddos <115896659+riddos@users.noreply.github.com>
2025-01-13 22:41:22 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
-<script>
-    document.addEventListener("DOMContentLoaded", () => {
-        const menuToggle = document.querySelector(".menu-toggle");
-        const menu = document.querySelector("nav ul.menu");
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector("nav ul.menu");
 
-        menuToggle.addEventListener("click", () => {
-            menu.classList.toggle("active");
-        });
+    menuToggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
     });
-</script>
+});
