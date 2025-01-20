dd1e0667477b20b3643303ce0bee5656c091682d
riddos <115896659+riddos@users.noreply.github.com>
2025-01-20 11:46:52 +0100

    Update istanbul.js

--- js/istanbul.js
+++ js/istanbul.js
@@ -47,3 +47,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuToggle = document.querySelector(".menu-toggle");
+        const menu = document.querySelector(".menu");
+
+        menuToggle.addEventListener("click", function () {
+            menu.classList.toggle("visible");
+        });
+    });
