5bffc2e28fb82bf05d798d49d1d01e764d771bee
riddos <115896659+riddos@users.noreply.github.com>
2025-01-20 11:07:10 +0100

    Update and rename index.js to inde

--- js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector("nav ul.menu");
-
-    menuToggle.addEventListener("click", () => {
-        menu.classList.toggle("active");
-    });
-});
