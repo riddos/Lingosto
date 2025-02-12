9908d4b5c50e6beaed4b761c0bea972e456d5610
riddos <115896659+riddos@users.noreply.github.com>
2025-02-12 20:11:34 +0100

    Update istanbul.js

--- js/istanbul.js
+++ js/istanbul.js
@@ -138,24 +138,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const testButton = document.getElementById('testButton');
     const flashcardsContainer = document.querySelector('.flashcards-container');
     const imageSection = document.querySelector('.image-section');
-    const textSection = document.querySelector('.text-section');
-    const dividers = document.querySelectorAll('.divider');
+    const translateTriggers = document.querySelectorAll('.translate-trigger');
     const quizSection = document.getElementById('quiz-section');
-    const answersButton = document.getElementById('answersButton');
 
     testButton.addEventListener('click', () => {
-        // Flashcard'ları, resmi, yazıyı ve divider'ları kaybet
+        // Flashcard'ları, resmi ve çeviri baloncuklarını kaybet
         flashcardsContainer.classList.add('fade-out');
         imageSection.classList.add('fade-out');
-        textSection.classList.add('fade-out');
-        dividers.forEach(divider => divider.classList.add('fade-out'));
+        translateTriggers.forEach(trigger => trigger.classList.add('fade-out'));
 
-        // Flashcard'ları, resmi, yazıyı ve divider'ları tamamen gizle
+        // Flashcard'ları, resmi ve çeviri baloncuklarını tamamen gizle
         setTimeout(() => {
             flashcardsContainer.classList.add('hidden');
             imageSection.classList.add('hidden');
-            textSection.classList.add('hidden');
-            dividers.forEach(divider => divider.classList.add('hidden'));
+            translateTriggers.forEach(trigger => trigger.classList.add('hidden'));
         }, 1000);
 
         // Quiz bölümünü göster
@@ -164,7 +160,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const correctAnswers = ["eşsiz", "tarihi", "sosyal", "adanmış", "fotoğrafçılara", "ilham"];
     
-    answersButton.addEventListener('click', () => {
+    function checkResults() {
         const selects = document.querySelectorAll("select");
         let correctCount = 0;
         
@@ -178,11 +174,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 select.classList.remove("correct");
             }
         });
-        
-        const successRate = (correctCount / correctAnswers.length) * 100;
-        document.getElementById("result").textContent = `Success rate: ${successRate.toFixed(0)}%`;
-    });
-});
         
         const successRate = (correctCount / correctAnswers.length) * 100;
         document.getElementById("result").textContent = `Success rate: ${successRate.toFixed(0)}%`;
