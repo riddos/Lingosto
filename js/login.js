f5a7a1b2d304cc374f01feff86f216402cc890c2
riddos <ridvanakdemirr@gmail.com>
2025-04-05 19:10:41 +0200

    loginlive

--- js/login.js
+++ js/login.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBwYxsjKgQWynx7k0iALM1U7p_KIOPeYVk",
@@ -14,31 +13,20 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM fully loaded and parsed");
-  const form = document.getElementById("register-form");
+  const form = document.getElementById("login-form");
 
   form.addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent form submission
 
-    const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(async (userCredential) => {
-        // User successfully registered
-        const user = userCredential.user;
-
-        // Save the username to Firestore
-        await setDoc(doc(db, "users", user.uid), {
-          username: username,
-          email: email,
-          createdAt: new Date().toISOString()
-        });
-
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // User successfully logged in
         window.location.href = "dashboard.html"; // Redirect to dashboard.html
       })
       .catch((error) => {
